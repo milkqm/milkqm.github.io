b4c26a1db6f2df00c847f8fc1fc92df294b074de
Phuong Nguyen <phuongnguyen@codeographer.net>
2019-03-31 12:17:22 +0700

    clean

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
     "use strict";
 
+    /*----------------------------------------------------*/
+    /*  Sticky header
+    /*----------------------------------------------------*/
     $(window).on("scroll", function() {
-        var stickyHeader = $(window).scrollTop() >= 80;
-        $(".header-area").toggleClass("header-fixed", stickyHeader);
+        var stickyHeader = $(window).scrollTop() > 105;
+        $(".header-area").toggleClass("sticky", stickyHeader);
     });
+
+    /*----------------------------------------------------*/
+    /*  Nice select
+    /*----------------------------------------------------*/
     $("select").niceSelect();
 
     /*----------------------------------------------------*/
@@ -103,208 +110,5 @@ $(document).ready(function() {
                 );
             }
         }
-    });
-
-    // Google Map
-    if (document.getElementById("contactMap")) {
-        google.maps.event.addDomListener(window, "load", init);
-
-        function init() {
-            // Basic options for a simple Google Map
-            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-            var mapOptions = {
-                // How zoomed in you want the map to start at (always required)
-                zoom: 11,
-
-                // The latitude and longitude to center the map (always required)
-                center: new google.maps.LatLng(40.67, -73.94), // New York
-
-                // How you would like to style the map.
-                // This is where you would paste any style found on Snazzy Maps.
-                styles: [{
-                        featureType: "water",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#e9e9e9"
-                        }, {
-                            lightness: 17
-                        }]
-                    },
-                    {
-                        featureType: "landscape",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#f5f5f5"
-                        }, {
-                            lightness: 20
-                        }]
-                    },
-                    {
-                        featureType: "road.highway",
-                        elementType: "geometry.fill",
-                        stylers: [{
-                            color: "#ffffff"
-                        }, {
-                            lightness: 17
-                        }]
-                    },
-                    {
-                        featureType: "road.highway",
-                        elementType: "geometry.stroke",
-                        stylers: [{
-                            color: "#ffffff"
-                        }, {
-                            lightness: 29
-                        }, {
-                            weight: 0.2
-                        }]
-                    },
-                    {
-                        featureType: "road.arterial",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#ffffff"
-                        }, {
-                            lightness: 18
-                        }]
-                    },
-                    {
-                        featureType: "road.local",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#ffffff"
-                        }, {
-                            lightness: 16
-                        }]
-                    },
-                    {
-                        featureType: "poi",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#f5f5f5"
-                        }, {
-                            lightness: 21
-                        }]
-                    },
-                    {
-                        featureType: "poi.park",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#dedede"
-                        }, {
-                            lightness: 21
-                        }]
-                    },
-                    {
-                        elementType: "labels.text.stroke",
-                        stylers: [{
-                                visibility: "on"
-                            },
-                            {
-                                color: "#ffffff"
-                            },
-                            {
-                                lightness: 16
-                            }
-                        ]
-                    },
-                    {
-                        elementType: "labels.text.fill",
-                        stylers: [{
-                                saturation: 36
-                            },
-                            {
-                                color: "#333333"
-                            },
-                            {
-                                lightness: 40
-                            }
-                        ]
-                    },
-                    {
-                        elementType: "labels.icon",
-                        stylers: [{
-                            visibility: "off"
-                        }]
-                    },
-                    {
-                        featureType: "transit",
-                        elementType: "geometry",
-                        stylers: [{
-                            color: "#f2f2f2"
-                        }, {
-                            lightness: 19
-                        }]
-                    },
-                    {
-                        featureType: "administrative",
-                        elementType: "geometry.fill",
-                        stylers: [{
-                            color: "#fefefe"
-                        }, {
-                            lightness: 20
-                        }]
-                    },
-                    {
-                        featureType: "administrative",
-                        elementType: "geometry.stroke",
-                        stylers: [{
-                            color: "#fefefe"
-                        }, {
-                            lightness: 17
-                        }, {
-                            weight: 1.2
-                        }]
-                    }
-                ]
-            };
-
-            // Get the HTML DOM element that will contain your map
-            // We are using a div with id="map" seen below in the <body>
-            var mapElement = document.getElementById("contactMap");
-
-            // Create the Google Map using our element and options defined above
-            var map = new google.maps.Map(mapElement, mapOptions);
-
-            // Let's also add a marker while we're at it
-            new google.maps.Marker({
-                position: new google.maps.LatLng(40.67, -73.94),
-                map: map,
-                title: "Snazzy!"
-            });
-        }
-    }
-
-    $("#mc_embed_signup").find("form").ajaxChimp();
-
-    // -------   Mail Send ajax
-    $(document).ready(function() {
-        var form = $("#myForm"); // contact form
-        var submit = $(".submit-btn"); // submit button
-        var alert = $(".alert-msg"); // alert div for show alert message
-
-        // form submit event
-        form.on("submit", function(e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: "mail.php", // form action url
-                type: "POST", // form submit method get/post
-                dataType: "html", // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html("Sending...."); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger("reset"); // reset form
-                    submit.attr("style", "display: none !important"); // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e);
-                }
-            });
-        });
     });
 });
